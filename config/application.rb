6e94ee885b9292e8442b7113c53fcc9e6ada829c
Oleg <darelvc@gmail.com>
2016-02-04 14:16:50 +0200

    Add ckeditor

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,34 @@ module KopilkaReceptov
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.precompile += %w(under_construction.css under_construction.js ie.js)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg)
+    assets_root = Rails.root.join("app/assets/images")
+    #paths = Dir[assets_root.join("/*.[gif|jpg|jpeg|png]")]
+    paths = Dir[Rails.root.join("app/assets/images/**/*")]
+    config.assets.precompile += paths
+
+    config.assets.precompile += %w(ckeditor)
+    config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.css)
+    config.assets.precompile += %w(ckeditor/lang/*.js)
+
+    config.assets.precompile += %w(rails_admin/colorpicker/*.gif rails_admin/colorpicker/*.png rails_admin/bootstrap/*.png rails_admin/aristo/images/* rails_admin/multiselect/*.png rails_admin/*.png)
+
+
+    config.assets.precompile += %w(ckeditor/config.js)
+    config.assets.precompile += %w(ckeditor/plugins/codemirror/plugin.js ckeditor/plugins/codemirror/lang/* ckeditor/plugins/codemirror/css/* ckeditor/plugins/codemirror/js/*.js ckeditor/plugins/codemirror/icons/* ckeditor/plugins/codemirror/theme/*)
+
+    config.generators do |g|
+       g.test_framework :rspec,
+                       fixtures: true,
+                       view_spec: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_spec: true
+       g.fixture_replacement :factory_girl, dir: 'spec/factories'
+
+    end
   end
 end
