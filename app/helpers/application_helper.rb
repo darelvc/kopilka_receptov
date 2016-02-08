6cbf7a96b4949f1cf9f8bb10e4bcdfe6a8318e5c
Oleg <darelvc@gmail.com>
2016-02-08 15:28:12 +0200

    Add Youtube video player

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,20 @@
 module ApplicationHelper
 
-	def link_to_in_li(body, url)
-	  active = "active" if current_page?(url)
-	  content_tag :li, class: active do
-	    link_to body, url
-	  end
-	end
+  def link_to_in_li(body, url)
+    active = "active" if current_page?(url)
+    content_tag :li, class: active do
+      link_to body, url
+    end
+  end
 
-	def title(text)
-	  content_for :title, text
-	end
+  def title(text)
+    content_for :title, text
+  end
+
+  def embed(youtube_url)
+    youtube_id = youtube_url.split("=").last
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+  end
+
 
 end
